f0c95f9caaf6ac1b229df596d03c3671a365f757
pranshu shah <pranshu.shah23@gmail.com>
2020-10-14 21:18:45 +0530

    todo and steps model added

---
+++ src/models/Steps.ts
@@ -0,0 +1,51 @@
+import { Schema, model, Document, Model, Types } from 'mongoose';
+interface stepsAttr {
+  taskTitle: string;
+  done: boolean;
+}
+// this will be type of document when create with new Step();
+export interface stepsDocInterface extends Document, stepsAttr {}
+
+// we extending Model because it will we need to add static types.
+interface stepsModel extends Model<stepsDocInterface> {
+  build(user: stepsAttr): stepsDocInterface;
+}
+
+const stepsSchema = new Schema(
+  {
+    taskTitle: {
+      type: String,
+      required: [true, 'taskTitle is required'],
+      validate: {
+        validator(taskTitle: string) {
+          return taskTitle.trim().length > 0;
+        },
+        message(message) {
+          return `${message.value} is not valid task title`;
+        },
+      },
+    },
+    done: {
+      type: Boolean,
+      required: [true, 'done is required'],
+    },
+  },
+  {
+    // to convert returning object as we want
+    toJSON: {
+      versionKey: false,
+      transform(doc: stepsDocInterface, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
+  },
+);
+
+// this created because we can type check with new Step({});
+stepsSchema.statics.build = function (step: stepsAttr) {
+  return new Step(step);
+};
+
+const Step = model<stepsDocInterface, stepsModel>('Steps', stepsSchema);
+export { Step, stepsAttr, stepsSchema };
---
+++ src/models/Todo.ts
@@ -0,0 +1,91 @@
+import { Schema, model, Document, Model, Types } from 'mongoose';
+import { stepsDocInterface, stepsSchema } from './Steps';
+interface todoAttr {
+  userId: Types.ObjectId;
+  todoTitle: string;
+  done: boolean;
+  normalTask?: boolean;
+  projectId?: Types.ObjectId;
+  dueDate?: Date;
+  important?: boolean;
+  myDay?: boolean;
+  steps?: stepsDocInterface[];
+}
+// this will be type of document when create with new Todo();
+export interface todoDocInterface extends Document, todoAttr {}
+
+// we extending Model because it will we need to add static types.
+interface todoModel extends Model<todoDocInterface> {
+  build(user: todoAttr): todoDocInterface;
+}
+
+const todoSchema = new Schema(
+  {
+    todoTitle: {
+      type: String,
+      required: [true, 'todoTitle is required'],
+      validate: {
+        validator(todoTitle: string) {
+          return todoTitle.trim().length > 0;
+        },
+        message(message) {
+          return `${message.value} is not valid todo title`;
+        },
+      },
+    },
+    userId: {
+      type: Types.ObjectId,
+      required: [true, 'userId is required '],
+    },
+    projectId: {
+      type: Types.ObjectId,
+    },
+    done: {
+      type: Boolean,
+      required: [true, 'done is required'],
+    },
+    important: {
+      type: Boolean,
+    },
+    myDay: {
+      type: Boolean,
+    },
+    dueDate: {
+      type: Date,
+    },
+    normalTask: {
+      type: Boolean,
+    },
+    steps: [stepsSchema],
+  },
+  {
+    // to convert returning object as we want
+    toJSON: {
+      versionKey: false,
+      transform(doc: todoDocInterface, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
+  },
+);
+
+// this created because we can type check with new Todo({});
+todoSchema.statics.build = function (todo: todoAttr) {
+  return new Todo(todo);
+};
+
+todoSchema.pre('validate', function (next) {
+  if (this.get('projectId') || this.get('normalTask')) {
+    next();
+  } else {
+    next(
+      new Error(
+        'you should provide either projectId or normalTaskShould be true',
+      ),
+    );
+  }
+});
+
+const Todo = model<todoDocInterface, todoModel>('Todo', todoSchema);
+export { Todo };
