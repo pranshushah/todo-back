f5f6f504f9a64e2e183b2832f083fffba188e48c
pranshu shah <pranshu.shah23@gmail.com>
2020-10-19 12:07:08 +0530

    geting tasks,all projects and tods project route

---
+++ src/routes/project/getAllProject.ts
@@ -0,0 +1,19 @@
+import express, { NextFunction, Request, Response } from 'express';
+import { authChecking } from '../../middleware/requireAuth';
+import { Project } from '../../models/Project';
+const router = express.Router();
+
+router.post(
+  '/api/project/getall',
+  authChecking,
+  async (req: Request, res: Response, next: NextFunction) => {
+    // getting all project by userId.
+    const project = await Project.find({
+      userId: req.user?.id,
+    });
+
+    res.status(200).send(project);
+  },
+);
+
+export { router as getAllProjectRoute };
---
+++ src/routes/todo/getTodosByProject.ts
@@ -0,0 +1,24 @@
+import express, { NextFunction, Request, Response } from 'express';
+import { authChecking } from '../../middleware/requireAuth';
+import { Todo } from '../../models/Todo';
+import { validateProjectTodo } from '../../middleware/projectValidateTodo';
+interface ReqTodoBody {
+  projectId: string;
+}
+
+const router = express.Router();
+
+router.patch(
+  '/api/todo/gettodosbyproject',
+  authChecking,
+  validateProjectTodo,
+  async (req: Request<any, any, ReqTodoBody>, res: Response) => {
+    const todos = await Todo.find({
+      projectId: req.body.projectId,
+      userId: req.user?.id,
+    });
+    res.status(200).send(todos);
+  },
+);
+
+export { router as getTodosByProjectsRoute };
