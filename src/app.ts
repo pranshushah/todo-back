1c74c954b91ec82fa9d77b441603f28b76a496bc
pranshu shah <pranshu.shah23@gmail.com>
2021-08-25 14:12:55 +0530

    React added

--- src/app.ts
+++ src/app.ts
@@ -64,10 +64,9 @@ app.use(getAllDueDateRoute);
 app.use(getAllImpRoute);
 app.use(getAllMyDayRoute);
 app.use(getAllTaskInProjectRoute);
-console.log(path.join(__dirname));
-// app.use(express.static(path.join(__dirname, 'build')));
-// app.get('/*', function (req, res) {
-//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
-// });
+app.use(express.static(path.join(__dirname, '..', 'build')));
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
+});
 app.use(errorHandler);
 export { app };
