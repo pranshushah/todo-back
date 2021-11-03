19a5dc594f08e8e1a2eda67566afb057787f1ed1
pranshu shah <pranshu.shah23@gmail.com>
2021-11-03 12:28:44 +0530

    fronend - nav added

--- src/routes/auth/currentUser_logout.ts
+++ src/routes/auth/currentUser_logout.ts
@@ -9,7 +9,7 @@ router.get('/api/current_user', (req: Request, res: Response) => {
 router.get('/api/logout', (req: Request, res: Response) => {
   req.logout();
   req.session = null;
-  res.status(200).send('done');
+  res.redirect('/');
 });
 
 export { router as currentUser_logout };
